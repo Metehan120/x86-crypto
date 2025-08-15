b43edafb9cf23255025887816c3b086ded1d6c2d
Metehan120 <metehan@zaferoglu.me>
2025-08-15 09:40:51 +0300

    SYS control

---
+++ src/memory/sys_control.rs
@@ -0,0 +1,64 @@
+#[cfg(target_os = "linux")]
+pub fn disable_dump() -> u8 {
+    use libc::{PR_SET_DUMPABLE, prctl};
+    use log::error;
+
+    if (unsafe { prctl(PR_SET_DUMPABLE, 0, 0, 0, 0) }) != 0 {
+        error!(
+            "Failed to disable dumpable: {}",
+            std::io::Error::last_os_error()
+        );
+        0
+    } else {
+        1
+    }
+}
+
+#[cfg(target_os = "linux")]
+pub fn disable_ptrace() -> u8 {
+    use libc::prctl;
+    use log::{error, info};
+    use std::io;
+
+    if unsafe {
+        use libc::PR_SET_PTRACER;
+        prctl(PR_SET_PTRACER, -1, 0, 0, 0)
+    } != 0
+    {
+        error!("Failed to disable ptrace: {}", io::Error::last_os_error());
+        0
+    } else {
+        info!("Ptrace disabled");
+        1
+    }
+}
+
+#[cfg(target_os = "linux")]
+pub fn lock_all_memory() -> u8 {
+    use libc::{MCL_CURRENT, MCL_FUTURE, mlockall};
+    use log::{error, info};
+    use std::io;
+
+    if unsafe { mlockall(MCL_CURRENT | MCL_FUTURE) } != 0 {
+        error!("mlockall failed: {}", io::Error::last_os_error());
+        0
+    } else {
+        info!("Process memory locked into RAM");
+        1
+    }
+}
+
+#[cfg(target_os = "linux")]
+pub fn unlock_all_memory() -> u8 {
+    use libc::munlockall;
+    use log::{error, info};
+    use std::io;
+
+    if (unsafe { munlockall() }) != 0 {
+        error!("munlockall failed: {}", io::Error::last_os_error());
+        0
+    } else {
+        info!("Process memory unlocked from RAM");
+        1
+    }
+}
